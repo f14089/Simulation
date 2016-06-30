6f7610b05374613ad67fa98fe1f9defbefaea90f
f14089 <f14089@gmail.com>
2016-06-30 21:58:08 +0800

    CORE init

---
+++ main.c
@@ -0,0 +1,11 @@
+/*Date : 2016/06/30*/
+/*Last Modify : Chien*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(){
+	printf("CORE : Clustering Optimization algorithm for Resource Efficiency");
+	printf("This file is used to implement CORE");
+	
+}
